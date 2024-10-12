f5a9785db15faf2fd03b72a5759c9b5efcd910fe
Jett Rogers <35665999+JettRog5026@users.noreply.github.com>
2024-10-12 12:39:10 -0500

    Create TankSpeedMiniboss.h

---
+++ TankSpeedMiniboss.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TankSpeedMiniboss.generated.h"
+
+UCLASS()
+class MAZECRAWLER_API ATankSpeedMiniboss : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATankSpeedMiniboss();
+
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	//Mesh Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)\
+		float MoveForce = 250.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)\
+		float JumpForce = 750.0f;
+
+public:	
+
+
+};
